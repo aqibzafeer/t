b2f2800e0333c387eb8458bb11420834a46c63ed
aqibzafeer <mail.aqibzafeer@gmail.com>
2025-10-12 07:23:31 -0700

    wqwq

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
 		<footer className="w-full border-t mt-8 mx-auto px-4 py-6 flex items-center justify-between text-sm text-white bg-red-800">
 				<div>© {new Date().getFullYear()} Copyright - LOGO </div>
 				<div className="flex items-center gap-4">
-					<a href="/about" className="hover:underline">About</a>
-					<a href="/contact" className="hover:underline">Contact</a>
+					{/* <a href="/about" className="hover:underline">About</a>
+					<a href="/contact" className="hover:underline">Contact</a> */}
 				</div>
 		</footer>
 	);
